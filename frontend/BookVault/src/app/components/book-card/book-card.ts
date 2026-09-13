dabaf57e68d5e8a6f182648e661667788b39e780
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-09-14 00:33:02 +0200

    feat: fetch books from API

--- frontend/BookVault/src/app/components/book-card/book-card.ts
+++ frontend/BookVault/src/app/components/book-card/book-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-book-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './book-card.html',
   styleUrl: './book-card.css',
 })
 export class BookCard {
 
+  @Input() book:any
 }
--- frontend/BookVault/src/app/components/dashboard/dashboard.ts
+++ frontend/BookVault/src/app/components/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { BookCard } from '../book-card/book-card';
+import { BookService } from '../../services/BookService';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +10,27 @@ import { BookCard } from '../book-card/book-card';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+
+  books:any[] = []
+
+  constructor(private booksservice : BookService){
+
+  }
+
+  ngOnInit(){
+
+    this.booksservice.getBooks().subscribe({
+
+      next:(data) => {
+
+        this.books = data
+      },
+
+      error:(error) =>{
+
+        console.error("Error loading books:",  error)
+      }
+    })
+  }
 
 }
